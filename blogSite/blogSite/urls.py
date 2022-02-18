e5a314b1a1039c94f96272d7b4936c0e1006bc7d
sharafx2qeshta <93531457+sharafx2qeshta@users.noreply.github.com>
2022-02-18 07:20:26 -0800

    Add files via upload

---
+++ blogSite/blogSite/urls.py
@@ -0,0 +1,37 @@
+"""blogSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap  # احا
+# i do the right import but i get  Import Error: ValueError: attempted relative import beyond top-level package
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
+]
+
+
+'''
+A web feed : is a data format (usually XML) that provides users 
+with the most recently updated content.
+'''
