fafa73798891ac40044eb814464216adf52b0f6e
sharafx2qeshta <93531457+sharafx2qeshta@users.noreply.github.com>
2022-02-18 07:26:10 -0800

    Add files via upload

---
+++ bookmarks/images/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
+
+'''
+you use the login_required decorator for the image_create
+view to prevent access for unauthenticated users.
+'''
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            # assign current user to the item
+            new_item.user = request.user
+            new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
+            messages.success(request, 'Image added successfully')
+            # redirect to new created item detail view
+            return redirect(new_item.get_absolute_url())
+
+    else:
+        # build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
+
+
+#  like or unlike view
+@ajax_required
+@login_required  # The login_required decorator prevents users who are not logged in from accessing this view.
+@require_POST  # The require_POST decorator returns an HttpResponseNotAllowed object (status code 405) if the HTTP
+# request is not done via POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+
+        except:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+'''
+require_GET decorator to only allow GET requests
+require_http_methods decorator to which you can pass a list of allowed methods 
+as an argument.
+
+'''
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images', 'images': images})
